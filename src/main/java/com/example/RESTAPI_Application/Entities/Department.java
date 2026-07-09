960b64bd3cb69ae2115b0781d0236117a014004f
fa22-bcs-088-svg <imaan2726@gmail.com>
2026-07-09 16:18:26 +0500

    Changes after Lomboq

--- src/main/java/com/example/RESTAPI_Application/Entities/Department.java
+++ src/main/java/com/example/RESTAPI_Application/Entities/Department.java
@@ -1,47 +1,27 @@
 package com.example.RESTAPI_Application.Entities;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long departmentId;
+    private Long Id;
 
     @NotBlank(message = "Department name is required")
     private String name;
 
     @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, orphanRemoval = true)
-    @JsonManagedReference
     private List<Course> courses = new ArrayList<>();
 
-    public Department() {}
-
-    public Long getId() {
-        return departmentId;
-    }
-    public void setId(Long id) {
-        this.departmentId = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public List<Course> getCourses() {
-        return courses;
-    }
-
-    public void setCourses(List<Course> courses) {
-        this.courses = courses;
-    }
 }
